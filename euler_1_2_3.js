1e5a4c85440f4e886522704f0f165e4f68f8b22b
Hans von Rautenfeld <djlittlemind@gmail.com>
2015-11-28 11:00:23 -0600

    all changes

--- euler_1_2_3.js
+++ euler_1_2_3.js
@@ -43,23 +43,62 @@ while (fib3 <4000000){
 //Project Euler 
 // 3 Largest prime factor
 
-var target= 13195;
-var recip = 0
-var possible = true;
+//var target= 13195;
+var target = 600851475143;
+var i = 1;
+var recip = target
+var possibleI = true;
+var possibleRecip = true;
+var largestPrimeFactor = 1;
 
-for (var i=2; i < Math.floor(target/2+1);i++){
-    if(target % i ===0){
+
+for (var i=1; i < Math.floor(target/2+1);i += 2){
+    
+        if(i <= recip){
+
+        //console.log("Testing: " + i);
+
+    if(target % i === 0){
         recip = target / i;
-        //console.log("Is factor: " + recip);
-        for(var j = 2; j < Math.floor(recip/2+1); j++){
-            if(recip % j === 0){
-                possible = false;
+        console.log("Is a factor (i): " + i);
+        console.log("Is a factor (recip): " + recip);
+
+       for(var j = 2; j < Math.floor(i/2+1); j++){
+            if(i % j === 0){
+                possibleI = false; //not prime
+                break;
                 }
             }
-            if(possible){
-                console.log("is prime: " + recip);
+        
+        for(var k = 2; k < Math.floor(recip/2+1); k++){
+            if(recip % k === 0){
+                possibleRecip = false; //not prime
+                //console.log(k);
                 break;
+                }
             }
+
+            if(possibleI){
+                console.log("Is a prime factor (i): " + i + "***");
+                if(largestPrimeFactor < i) {
+                largestPrimeFactor = i;
+                }
+            }        
+            if(possibleRecip){
+                console.log("Is a prime factor (recip): " + recip + "***");
+                if(largestPrimeFactor < recip) {
+                largestPrimeFactor = recip;
+                }
+            }        
+
+            
+            
     }
-    possible = true;
+
+
+    possibleI = true;
+    possibleRecip = true;
 }
+console.log("The largest prime factor is: " + largestPrimeFactor + "*****");
+}
+
